8df0338c22922e2bac3b4f5798fdb316c38daf66
safebear <student@safebear.co.uk>
2019-07-23 15:04:18 +0100

    update

---
+++ src/test/java/co/uk/safebear/kennals/Cost.java
@@ -0,0 +1,16 @@
+package co.uk.safebear.kennals;
+
+public class Cost {
+
+    private static double value;
+
+public static void setValue(double newValue){
+
+    value = newValue;
+}
+
+public static double getValue(){
+
+    return value;
+}
+}
