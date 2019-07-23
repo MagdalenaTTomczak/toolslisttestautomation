d8fb176c5424977204d2f3301a8c021e0022ef1a
safebear <student@safebear.co.uk>
2019-07-23 10:59:50 +0100

    step def update

--- src/test/java/co/uk/safebear/Stepdefs.java
+++ src/test/java/co/uk/safebear/Stepdefs.java
@@ -24,5 +24,45 @@ public class Stepdefs {
         // Write code here that turns the phrase above into concrete actions
         throw new cucumber.api.PendingException();
     }
+    @Given("the tool list is displayed")
+    public void the_tool_list_is_displayed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
 
+    @When("the user selects the tool")
+    public void the_user_selects_the_tool() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the tool detail page is displayed")
+    public void the_tool_detail_page_is_displayed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("the user search for a term {string}")
+    public void the_user_search_for_a_term(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("list of tools related to that term is returned")
+    public void list_of_tools_related_to_that_term_is_returned() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("the user selects the delete option")
+    public void the_user_selects_the_delete_option() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the tool is deleted")
+    public void the_tool_is_deleted() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
 }
