e52462c4e1abdd20f05fd4273af339834e01fbcf
safebear <student@safebear.co.uk>
2019-07-24 12:34:16 +0100

    step def updates

--- src/test/java/co/uk/safebear/Stepdefs.java
+++ src/test/java/co/uk/safebear/Stepdefs.java
@@ -6,7 +6,6 @@ import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import jdk.internal.org.objectweb.asm.tree.TryCatchBlockNode;
 import org.openqa.selenium.WebDriver;
 
 import static org.junit.Assert.*;
