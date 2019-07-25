ef0643d82cbebbada8ec46bfe89aec39c71c639d
magdalena <magdalena.tomczak@mubaloo.com>
2019-07-25 09:35:52 +0100

    tools page update

--- src/test/java/co/uk/safebear/pages/locators/ToolsPageLocators.java
+++ src/test/java/co/uk/safebear/pages/locators/ToolsPageLocators.java
@@ -9,4 +9,14 @@ public class ToolsPageLocators {
     // messages
     private By successfullLoginMessage = By.xpath(".//body/div[@class='container']/p/b");
 
+    // Locator for the search field
+    private By searchFieldLocator = By.id("toolName");
+
+    // Locator for the search button
+    private By searchButtonLocator = By.name(".//button[.=\'Search\']");
+
+    // Locator for the name of the tool which is returned - may I use "searchTerm" instead of Selenium ??
+
+    private By returnedToolName = By.xpath(".//tr/td[.='Selenium']");
+
 }
